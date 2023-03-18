9a37c3a48c2c720d57f1fb0110ae8faf12649703
Angshuman Barpujari <angshuarin@gmail.com>
2023-03-18 23:49:46 +0530

    custom app icon added

---
+++ lib/core/icon/app_icon.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+
+class AppIcon {
+  AppIcon._();
+
+  static const _kFontFam = 'AppIcon';
+  static const String? _kFontPkg = null;
+
+  static const IconData laPrescriptionBottle = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData mdiClockTimeSevenOutline = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData uilHomeAlt = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData uilSetting = IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData carbonShoppingCart = IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
