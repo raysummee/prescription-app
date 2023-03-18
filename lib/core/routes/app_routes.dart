ec9d9291cd1e4f3aba66436744bb821093b47517
Angshuman Barpujari <angshuarin@gmail.com>
2023-03-18 23:50:13 +0530

    app route added wrapper scaffold

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -2,6 +2,7 @@ import 'package:app/features/home/presentation/pages/home_page.dart';
 import 'package:app/features/login/presentation/bloc/authentication/authentication_bloc.dart';
 import 'package:app/features/login/presentation/pages/login_page.dart';
 import 'package:app/features/login/presentation/pages/register_page.dart';
+import 'package:app/features/main/presentation/pages/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
@@ -18,29 +19,76 @@ class AppRoutes{
     routes: [
       GoRoute(
         path: "/",
+        redirect: (context, state) {
+          final isAuthenticated = context.read<AuthenticationBloc>().state is AuthenticationSuccess;
+          if(isAuthenticated){
+            return "/home";
+          }
+          return null;
+        },
         builder: (context, state) {
-          return BlocConsumer<AuthenticationBloc, AuthenticationState>(
-            buildWhen: (previous, current) {
-              return previous is AuthenticationInitial;
-            },
+          return BlocListener<AuthenticationBloc, AuthenticationState>(
             listener: (context, state) {
               if(state is AuthenticationFailure){
                 context.go("/login");
               }
-            },
-            builder: (context, state) {
               if(state is AuthenticationSuccess){
-                return const HomePage();
+                context.go("/home");
               }
-              return const Scaffold(
-                body: Center(
-                  child: CircularProgressIndicator(),
-                ),
-              );
             },
+            child: const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
           );
         },
       ),
+      ShellRoute(
+        pageBuilder: (context, state, child) {
+          return CustomTransitionPage(
+            transitionsBuilder: (context, animation, secondaryAnimation, child) {
+              return FadeTransition(opacity: animation, child: child);
+            },
+            child: MainPage(
+              child: child
+            )
+          );
+        },
+        routes: [
+          GoRoute(
+            path: "/home",
+            pageBuilder: (context, state) => const NoTransitionPage(
+              child: HomePage()
+            ),
+          ),
+          GoRoute(
+            path: "/timeline",
+            pageBuilder: (context, state) => const NoTransitionPage(
+              child: Center(child: Text("Timeline"))
+            ),
+          ),
+          GoRoute(
+            path: "/prescription",
+            pageBuilder: (context, state) => const NoTransitionPage(
+              child: Center(child: Text("prescription"))
+            ),
+          ),
+          GoRoute(
+            path: "/cart",
+            pageBuilder: (context, state) => const NoTransitionPage(
+              child: Center(child: Text("cart"))
+            ),
+          ),
+          GoRoute(
+            path: "/settings",
+            pageBuilder: (context, state) => const NoTransitionPage(
+              child: Center(child: Text("settings"))
+            ),
+          ),
+        ]
+      ),
+
       GoRoute(
         path: '/register',
         pageBuilder: (context, state) {
